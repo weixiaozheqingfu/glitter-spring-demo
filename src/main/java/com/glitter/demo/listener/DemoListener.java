bc287b2884451f0973ecd99dba62872e73fd1fdb
limengjun <limengjun@syswin.com>
2018-04-24 18:12:35 +0800

    提交修改

--- src/main/java/com/glitter/demo/listener/DemoListener.java
+++ src/main/java/com/glitter/demo/listener/DemoListener.java
@@ -16,6 +16,10 @@ public class DemoListener implements ServletContextListener {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    // TODO 怎么打印出特定包的日志  比如mybatis日志，比如数据库日志。
+
+    // TODO 可以for遍历10000条日志打印，来观察异步日志的输出情况，可与同步日志输出情况做对比。
+
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         String projectName = String.valueOf(servletContextEvent.getServletContext().getInitParameter("projectName"));
