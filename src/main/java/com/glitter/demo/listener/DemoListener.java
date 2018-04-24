2f46159eb8fb8617b7871a6fa0b9526176d5b47b
limengjun <limengjun@syswin.com>
2018-04-24 16:23:34 +0800

    提交修改

--- src/main/java/com/glitter/demo/listener/DemoListener.java
+++ src/main/java/com/glitter/demo/listener/DemoListener.java
@@ -1,9 +1,9 @@
 package com.glitter.demo.listener;
 
-
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -12,9 +12,9 @@ import javax.servlet.ServletContextListener;
  * Created by Administrator on 2018/4/23.
  */
 public class DemoListener implements ServletContextListener {
-    private static final Logger logger = LogManager.getLogger(DemoListener.class);
+//  private static final Logger logger = LogManager.getLogger(DemoListener.class);
 
-
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
@@ -24,7 +24,7 @@ public class DemoListener implements ServletContextListener {
         logger.info("===========================info================================");
         logger.warn("===========================warn================================");
         logger.error("===========================error================================");
-        logger.fatal("===========================fatal================================");
+//      logger.fatal("===========================fatal================================");
     }
 
     @Override
