88f64eb340213d7a0855a8e6d09f12dd86c1ceac
limengjun <limengjun@syswin.com>
2018-04-27 09:19:37 +0800

    提交修改

--- src/main/java/com/glitter/demo/service/impl/PositionGroupServiceImpl.java
+++ src/main/java/com/glitter/demo/service/impl/PositionGroupServiceImpl.java
@@ -48,6 +48,13 @@ public class PositionGroupServiceImpl implements IPositionGroupService{
 
     /**
      * 方式二:通过接口new对应的mapper.xml实现类的方式
+     * // TODO 这里留一个问题，即如果该实现类是单例的情况下，高并发时，有两个用户同时调用该实例的两个不同的方法，如何保证session不串。
+     * // 即第一个方法刚获取到一个session1，然后基于session1得到了mapper对象，但是方法没往下继续走，此时线程切换到另外一个方法，session又重新获取成了session2，那么
+     * // 此时线程切换到第一个方法，那么第一个方法是不是就变成使用session2了，此时方法一执行session.close的时候，会不会将方法二中的session也关闭了，因为是同一个对象啊。
+     * // 更可怕的是事务问题，事务开启和事务提交及事务回滚，都是基于connection对象的，也就是基于mybatis封装的session对象的，如果session在一个方法执行过程中被重新赋值，
+     * // 那么当这个方法执行事务开启和事务提交或者事务回滚使不是基于同一个session，事务就会有问题。
+     * // 但是spring的aop又是如何做到这一切呢，看上去，他也是基于单例的，这个问题值得后续继续研究。有更多的问题需要研究，暂时先搁置此问题，不能一叶障目，就停滞不前，那样将会永远裹足不前。
+     * // 知道哪里会，哪里不会也是一种能力，暂时放一放也是一种智慧。
      * @param positionGroupId
      * @return
      */
